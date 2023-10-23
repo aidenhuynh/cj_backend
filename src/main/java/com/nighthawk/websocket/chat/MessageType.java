2044e017aa81d54471e655d274595a54a7fc48cc
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-10-23 16:22:32 -0700

    added websocket dependencies

---
+++ src/main/java/com/nighthawk/websocket/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.nighthawk.websocket.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/nighthawk/websocket/config/WebSocketEventListener.java
@@ -0,0 +1,37 @@
+package com.nighthawk.websocket.config;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
+
+import com.nighthawk.websocket.chat.ChatMessage;
+import com.nighthawk.websocket.chat.MessageType;
+
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+    
+    private final SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocletDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null){
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                .type(MessageType.LEAVE)
+                .sender(username)
+                .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
