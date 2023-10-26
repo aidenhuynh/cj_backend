d8526726506e3023a08513eb6f2d666e4f15ee55
KKcbal <tabbed.out3321@gmail.com>
2023-10-26 12:03:59 -0700

    I HATE STATISTICS (im deleting it all

--- src/main/java/com/nighthawk/spring_portfolio/mvc/statistics/StatisticsRoleJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.statistics;
-
-import java.util.Date;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  StatisticsRoleJpaRepository extends JpaRepository<StatisticsRole, Long> {
-    StatisticsRole findByDob(Date dob);
-}
