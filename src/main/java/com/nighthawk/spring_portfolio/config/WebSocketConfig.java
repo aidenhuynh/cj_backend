fbe00c2a8b7d863246812bbb925e5d314f4284ec
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-10-26 08:52:19 -0700

    added it back

--- src/main/java/com/nighthawk/spring_portfolio/config/WebSocketConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/config/WebSocketConfig.java
@@ -2,19 +2,17 @@ package com.nighthawk.spring_portfolio.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@CrossOrigin
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }  
 
     @Override
