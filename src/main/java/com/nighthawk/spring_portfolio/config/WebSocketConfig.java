0ef4b1d1cc2574511d02fb8327eb9f7b37d64c32
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-10-27 09:39:30 -0700

    added a handshake handler?

--- src/main/java/com/nighthawk/spring_portfolio/config/WebSocketConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/config/WebSocketConfig.java
@@ -1,19 +1,32 @@
 package com.nighthawk.spring_portfolio.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
-    @Override
+    @Bean
+    public DefaultHandshakeHandler handshakeHandler() {
+        return new DefaultHandshakeHandler(new TomcatRequestUpgradeStrategy());
+    }
+
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("toby-leeder.github.io").withSockJS();
-    }  
+        registry.addEndpoint("/ws")
+                .setHandshakeHandler(handshakeHandler())
+                .setAllowedOrigins("toby-leeder.github.io")
+                .withSockJS();
+    }
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
