38658f3a0a1660bb3a5c8d1d1ac7016a27b73c61
Toby-Leeder <tobyzeavleeder@gmail.com>
2023-10-27 09:17:39 -0700

    more origin stuff

--- src/main/java/com/nighthawk/spring_portfolio/config/WebSocketConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("toby-leeder.github.io").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("toby-leeder.github.io").withSockJS();
     }  
 
     @Override
